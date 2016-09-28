0ebe8165e5e3732b17613acdc859297d6c79db73
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-09-28 16:28:34 +0300

    1

--- MethodsTask3/MethodsTask3/MethodsTask3/Program.cs
+++ MethodsTask3/MethodsTask3/MethodsTask3/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OperationsTask1
+namespace MethodsTask3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Creater creater = new Creater();
+            SmartClass sc1 = new SmartClass();
+            SmartClass sc2 = new SmartClass();
+            SmartClass sc3 = new SmartClass();
+            sc3.Dispose();
+            Console.WriteLine(SmartClass.Counter);
             Console.ReadKey();
         }
     }
--- MethodsTask3/MethodsTask3/MethodsTask3/Properties/AssemblyInfo.cs
+++ MethodsTask3/MethodsTask3/MethodsTask3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OperationsTask1")]
+[assembly: AssemblyTitle("MethodsTask3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("OperationsTask1")]
+[assembly: AssemblyProduct("MethodsTask3")]
 [assembly: AssemblyCopyright("Copyright © EPAM Systems 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ddac9171-53ef-46fd-ad5e-40f734eeede7")]
+[assembly: Guid("b92ac876-5760-49d4-aa4f-3ad8018e3c01")]
 
 // Version information for an assembly consists of the following four values:
 //

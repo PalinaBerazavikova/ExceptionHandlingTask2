9d9a34fa9eb1219945c9274385a53a2e541a135b
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-10-07 14:24:08 +0300

    123

---
+++ ExceptionHandlingTask2/ExceptionHandlingTask2/MyConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingTask2
+{
+    public static class MyConverter
+    {
+        public static int MyTryParse(string str)
+        {
+            int number=0;
+            str = str.Trim();
+            Regex rgx = new Regex(@"^[+-]?\d+$");
+            if (rgx.IsMatch(str))
+            {
+                return number = MyConverter.ConvertToInt(str);
+            }
+            return number;
+
+        }
+        private static int ConvertToInt(string str)
+        {
+            int count = str.Length;
+            int number = 0;
+            foreach(char digit in str)
+            {
+                number += (int)char.GetNumericValue(digit) * (int)Math.Pow(10, count);
+                count--;
+            }
+            return number;
+        }
+    }
+}
---
+++ ExceptionHandlingTask2/TestMyConverter/TestConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExceptionHandlingTask2;
+
+namespace TestMyConverter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int res;
+            Assert.IsTrue(MyConverter.MyTryParse("35 3453", out res));
+           
+        }
+    }
+}

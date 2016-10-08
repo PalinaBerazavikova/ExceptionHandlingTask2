8123be9f5353b022754d70c051202201e5cb01e4
User <User@DESKTOP-K2AOAA8>
2016-10-08 10:47:32 +0300

    234

--- ExceptionHandlingTask2/ExceptionHandlingTask2/MyConverter.cs
+++ ExceptionHandlingTask2/ExceptionHandlingTask2/MyConverter.cs
@@ -9,26 +9,49 @@ namespace ExceptionHandlingTask2
 {
     public static class MyConverter
     {
-        public static int MyTryParse(string str)
+        public static int MyParse(string str)
         {
-            int number=0;
+            int number = 0;
             str = str.Trim();
+            if (str.Contains("."))
+            {
+                str = str.Remove(str.IndexOf("."));
+            }
+            if (str.Contains(","))
+            {
+                str = str.Remove(str.IndexOf(","));
+            }
+            char sym = '+';
             Regex rgx = new Regex(@"^[+-]?\d+$");
             if (rgx.IsMatch(str))
             {
-                return number = MyConverter.ConvertToInt(str);
+                if (!char.IsDigit(str.First()))
+                {
+                    sym = str.First();
+                    number = ConvertToInt(str.Substring(1, str.Length - 1));
+                    return sym == '-' ? 0 - number : number;
+                }
+                else
+                {
+                    return number = MyConverter.ConvertToInt(str);
+                }
+            }else
+            {
+                throw new WrongFormatException("Value is not valid");
             }
             return number;
-
         }
         private static int ConvertToInt(string str)
         {
-            int count = str.Length;
+            int count = str.Length - 1;
             int number = 0;
-            foreach(char digit in str)
+            checked
             {
-                number += (int)char.GetNumericValue(digit) * (int)Math.Pow(10, count);
-                count--;
+                foreach (char digit in str)
+                {
+                    number += (int)char.GetNumericValue(digit) * (int)Math.Pow(10, count);
+                    count--;
+                }
             }
             return number;
         }
--- ExceptionHandlingTask2/TestMyConverter/TestConverter.cs
+++ ExceptionHandlingTask2/TestMyConverter/TestConverter.cs
@@ -5,14 +5,65 @@ using ExceptionHandlingTask2;
 namespace TestMyConverter
 {
     [TestClass]
-    public class UnitTest1
+    public class TestConverter
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestParse1()
         {
-            int res;
-            Assert.IsTrue(MyConverter.MyTryParse("35 3453", out res));
-           
+            TestParse(353453, MyConverter.MyParse("353453"));
+        }
+        [TestMethod]
+        public void TestParse2()
+        {
+            TestParse(353453, MyConverter.MyParse("+353453"));
+        }
+        [TestMethod]
+        public void TestParse3()
+        {
+            TestParse(-353453, MyConverter.MyParse("-353453"));
+        }
+        public void TestParse(int exp, int act)
+        {
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestBigValue1()
+        {
+            TestValue("356800000000");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestBigValue2()
+        {
+            TestValue("-356800000000");
+        }
+
+        public void TestValue(string str)
+        {
+            MyConverter.MyParse(str);
+        }
+
+        [TestMethod]
+        public void TestDoubleValue1()
+        {
+            TestParse(3568000, MyConverter.MyParse("3568000,00000"));
+        }
+        [TestMethod]
+        public void TestDoubleValue2()
+        {
+            TestParse(3568000, MyConverter.MyParse("3568000.00000"));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(WrongFormatException))]
+        public void TestWrongValue3()
+        {
+            TestWrongValue("asdf");
+        }
+        public void TestWrongValue(string str)
+        {
+            MyConverter.MyParse(str);
         }
     }
 }
